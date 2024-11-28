71d2ab5dc3986e0a37a58b2be45bec7336ea656f
Sushant Lama <sushantlama732@gmail.com>
2024-11-28 20:55:43 +0545

    added custom resposne

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const dotenv = require("dotenv").config();
-
+const app = require("./app");
 
 const connectDB = require("./db");
 
-connectDB();
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+  })
+})
+.catch((err) => {
+  console.log("MONGO db connection failed !!! ", err);
+})
---
+++ src/utils/apiError.js
@@ -0,0 +1,18 @@
+class APIERROR extends Error {
+    constructor(statusCode, message="something wrong", errors=[], stack){
+        super(message)
+            this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = errors
+        if (stack) {
+            this.stack = stack
+        } else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+        
+    }
+}
+
+module.exports = APIERROR
